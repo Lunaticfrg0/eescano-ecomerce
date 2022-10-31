dd3bb955e121325d0f7fc14ba041e5598085432d
Lunaticfrg0 <emilio3scano@gmail.com>
2022-10-31 18:33:51 -0400

    create sign in with creds and styled the auth page

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -1,5 +1,75 @@
-const SignInForm = () => {
+import { useState } from "react";
+import { createUserDocumentFromAuth, signInAuthUserWithEmailAndPassword, signInWithGooglePopUp } from "../../utils/firebase/firebase.utils";
+import Button from "../button/button.component";
+import FormInput from "../form-input/form-input.component";
+import "./sign-in-form.styles.scss"
 
+const defaultFormFields = {
+    email: '',
+    password:'',
+}
+
+const SignInForm = () => {
+    const [formFields, setFormFields] = useState(defaultFormFields)
+    const { email, password } = formFields;
+
+    const handleChange = (event) => {
+        const {name, value} = event.target;
+        setFormFields({...formFields, [name]: value})
+    }
+
+    const resetFormFields = () => {
+        setFormFields(defaultFormFields)
+    }
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        if(!email || !password){
+            alert("All values should be added")
+            return
+        }
+        try {
+            const response = await signInAuthUserWithEmailAndPassword(email, password)
+            resetFormFields()
+        } catch (error) {
+            switch(error.code){
+                case "auth/wrong-password":
+                    alert("Email or password incorrect")
+                    break;
+                case "auth/user-not-found":
+                    alert("Not user associates to email")
+                    break;
+                default:
+                    console.log(error.code)
+                    break;
+            }
+        }
+
+    }
+
+    const signInWithGoogle = async () => {
+        const {user} = await signInWithGooglePopUp();
+        createUserDocumentFromAuth(user)    
+    }
+
+    return(
+        <div className="sign-up-container">
+            <h2>Already have an account?</h2>
+            <span>Sign in with your email and password or Gmail</span>
+            <form onSubmit={handleSubmit}>
+
+                <FormInput label="Email" type="email" name="email" onChange={handleChange} required value={email}/>
+
+                <FormInput label="Password" type="password" name="password" onChange={handleChange} required value={password}/>
+                
+
+               <div className="buttons-container">
+                <Button type="submit">Sign In</Button>
+                <Button onClick={signInWithGoogle} buttonType="google" type="button">Google sign in </Button>
+               </div>
+            </form>
+        </div>
+    )
 }
 
 export default SignInForm;
--- src/routes/authentication/authentication.component.jsx
+++ src/routes/authentication/authentication.component.jsx
@@ -1,20 +1,12 @@
+import SignInForm from "../../components/sign-in-form/sign-in-form.component";
 import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
-import {
-    signInWithGooglePopUp, 
-    createUserDocumentFromAuth,
-     } from "../../utils/firebase/firebase.utils"
 
 import "./authentication.styles.scss"
 
 const Authentication = () => {
-    const logGoogleUser = async () => {
-        const {user} = await signInWithGooglePopUp();
-        createUserDocumentFromAuth(user)    
-    }
     return (
-        <div>
-            <h1>Sign In</h1>
-            <button onClick={logGoogleUser}>Sign in google</button>
+        <div className="authentication-container">
+            <SignInForm/>
             <SignUpForm/>
         </div>
     )

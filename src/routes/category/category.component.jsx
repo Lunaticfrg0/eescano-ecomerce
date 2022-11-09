6f35374df83f5758866a9d89a6d9805fdf8ddc37
Lunaticfrg0 <emilio3scano@gmail.com>
2022-11-09 16:38:02 -0400

    changes the rest of scss files

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,4 +1,4 @@
-import "./category.styles.scss"
+import { CategoryContainer, Title } from './category.styles';
 import { useParams } from "react-router-dom";
 import { useContext, useState, useEffect, Fragment } from "react";
 import { CategoriesContext } from "../../contexts/categories.context";
@@ -14,13 +14,13 @@ const Category = () => {
 
     return (
         <Fragment>
-            <h2 className="category-title">{category.toUpperCase()}</h2>
-            <div className="category-container">
+            <Title>{category.toUpperCase()}</Title>
+            <CategoryContainer>
                 {
                     products &&
                     products.map((product) => <ProductCard key={product.id} product={product}/>)
                 }  
-            </div>
+            </CategoryContainer>
         </Fragment>
     )
 }

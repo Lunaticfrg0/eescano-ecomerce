4912ccb1b1ff560213bb842f964ab9a45d140e5f
Lunaticfrg0 <emilio3scano@gmail.com>
2022-11-22 16:50:07 -0400

    add redux and change user and category to redux

--- src/routes/navigation-bar/navigation-bar.component.jsx
+++ src/routes/navigation-bar/navigation-bar.component.jsx
@@ -1,7 +1,6 @@
 import { Outlet } from "react-router-dom"
 import { Fragment, useContext} from "react";
 import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
-import { UserContext } from "../../contexts/user.context";
 import { signOutUser } from "../../utils/firebase/firebase.utils";
 import CartIcon from "../../components/cart-icon/cart-icon.component";
 import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
@@ -9,8 +8,11 @@ import { CartContext } from "../../contexts/cart.context";
 
 import { NavigationContainer, NavLink, NavLinks, LogoContainer } from "./navigation-bar.styles";
 
+import { useSelector } from "react-redux";
+import { selectCurrentUser } from "../../store/user/user.selector";
+
 const NavigationBar = () => {
-    const {currentUser} = useContext(UserContext)
+    const currentUser =  useSelector(selectCurrentUser)
     const {isCartOpen} = useContext(CartContext)
     const signOutHandler = async () => {
         await signOutUser()
--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,8 +1,20 @@
 import { Routes, Route} from "react-router-dom"
 import CategoriesPreview from "../categories-preview/categories-preview.component";
 import Category from "../category/category.component";
+import { useEffect } from "react";
+import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.utils";
+import { setCategoriesMap } from "../../store/categories/category.action";
+import { useDispatch } from "react-redux";
 
 const Shop = () => {
+    const dispatch = useDispatch();
+    useEffect(() => {
+        const getCategories = async () => {
+         const categoriesMap = await  getCategoriesAndDocuments()
+         dispatch(setCategoriesMap(categoriesMap))
+        }
+        getCategories();
+     }, [])
     return(
         <Routes>
             <Route index element={<CategoriesPreview/>}/>

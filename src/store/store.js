46e21f86f9de72854e0361647a0cf32073b4aa57
Lunaticfrg0 <emilio3scano@gmail.com>
2022-11-23 17:36:11 -0400

    add persistance using rdux persist

--- src/store/store.js
+++ src/store/store.js
@@ -1,11 +1,26 @@
 import { compose, createStore, applyMiddleware } from "redux"
 import logger from "redux-logger";
-
-
+import storage from 'redux-persist/lib/storage'
+import { persistStore, persistReducer } from "redux-persist"
 import { rootReducer } from "./root-reducer";
 
-const middleWares = [logger]
+const persistConfig = {
+    key: 'root',
+    storage,
+    blacklist: ['user']
+}
+const composeEnhancer =
+  (process.env.NODE_ENV !== 'production' &&
+    window &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose;
+  
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 
-const composeEnhancers = compose(applyMiddleware(...middleWares))
+const middleWares = [process.env.NODE_ENV !== "production" && logger].filter(Boolean)
 
-export const store = createStore(rootReducer, undefined, composeEnhancers)
+const composeEnhancers = composeEnhancer(applyMiddleware(...middleWares))
+
+export const store = createStore(persistedReducer, undefined, composeEnhancers)
+
+export const persistor = persistStore(store);

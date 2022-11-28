a4c57b98bbe569098012389736d19511e5056181
Lunaticfrg0 <emilio3scano@gmail.com>
2022-11-28 16:26:07 -0400

    change from thunk to saga

--- src/store/categories/category.action.js
+++ src/store/categories/category.action.js
@@ -1,6 +1,5 @@
 import { createAction } from "../../utils/reducer/reducer.utils"
 import { CATEGORIES_ACTION_TYPES } from "./category.types"
-import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.utils"
 
 export const fetchCategoriesStart = () => 
     createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START)
@@ -10,13 +9,3 @@ export const fetchCategoriesSuccess = (caregoriesArray) =>
 
 export const fetchCategoriesError = (error) => 
     createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_ERROR, error)
-
-export const fetchCategoriesAsync = () => async (dispatch) => {
-    dispatch(fetchCategoriesStart())
-    try {
-        const categoriesArray = await getCategoriesAndDocuments()
-        dispatch(fetchCategoriesSuccess(categoriesArray))
-    } catch (error) {
-        dispatch(fetchCategoriesError(error))
-    }
-}
--- src/store/store.js
+++ src/store/store.js
@@ -3,8 +3,9 @@ import logger from "redux-logger";
 import storage from 'redux-persist/lib/storage'
 import { persistStore, persistReducer } from "redux-persist"
 import { rootReducer } from "./root-reducer";
-import thunk from "redux-thunk"
-
+// import thunk from "redux-thunk"
+import { rootSaga } from "./root-saga";
+import createSagaMiddleware from "redux-saga";
 const persistConfig = {
     key: 'root',
     storage,
@@ -16,12 +17,18 @@ const composeEnhancer =
     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
   compose;
   
+const sagaMiddleware = createSagaMiddleware()
 const persistedReducer = persistReducer(persistConfig, rootReducer)
 
-const middleWares = [process.env.NODE_ENV !== "production" && logger, thunk].filter(Boolean)
+const middleWares = [process.env.NODE_ENV !== "production" && logger, 
+sagaMiddleware
+//thunk
+].filter(Boolean)
 
 const composeEnhancers = composeEnhancer(applyMiddleware(...middleWares))
 
 export const store = createStore(persistedReducer, undefined, composeEnhancers)
+
+sagaMiddleware.run(rootSaga)
 
 export const persistor = persistStore(store);

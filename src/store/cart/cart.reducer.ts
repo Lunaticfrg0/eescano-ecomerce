79bdf58193511ffa5d1cff8e7db023b1e5109dec
Emilio Escaño <52083070+Lunaticfrg0@users.noreply.github.com>
2023-01-16 18:48:15 -0400

    add changes to turn project into ts

---
+++ src/store/cart/cart.reducer.ts
@@ -0,0 +1,33 @@
+import { CartItem} from "./cart.types"
+import { AnyAction } from "redux"
+
+import { setCartItems, setIsCartOpen } from "./cart.action"
+
+
+export type CartState = {
+    cartItems: CartItem[];
+    isCartOpen: boolean
+}
+
+export const CART_INITIAL_STATE: CartState = {
+    isCartOpen: false,
+    cartItems: [],
+}
+
+export const cartReducer = (state = CART_INITIAL_STATE, action: AnyAction) => {
+    if(setIsCartOpen.match(action))
+    {
+        return {
+                    ...state,
+                    isCartOpen: action.payload
+                }
+    }
+    if(setCartItems.match(action))
+    {
+        return {
+                    ...state,
+                    cartItems: action.payload
+                }
+    }
+    return state;
+}

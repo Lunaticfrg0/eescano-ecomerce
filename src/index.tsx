4e23ddbe65d3736652908ca0828c01d83f35ade9
Emilio Escaño <52083070+Lunaticfrg0@users.noreply.github.com>
2023-01-26 17:24:30 -0400

    add files and finished pwa

--- src/index.tsx
+++ src/index.tsx
@@ -8,6 +8,8 @@ import { store, persistor } from "./store/store";
 import { PersistGate } from "redux-persist/integration/react"
 import { Elements } from "@stripe/react-stripe-js";
 import { stripePromise } from "./utils/stripe/stripe.utils";
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );root.render(
@@ -23,7 +25,7 @@ const root = ReactDOM.createRoot(
     </Provider>
   </React.StrictMode>
 );
-
+serviceWorkerRegistration.unregister();
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
